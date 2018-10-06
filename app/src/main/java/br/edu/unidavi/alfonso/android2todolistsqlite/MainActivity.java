a06d8d0e0546fb6eff3f38dd464d03df8ab70be2
alfonsojunior <alfonsojunior@gmail.com>
2018-10-06 08:20:25 -0300

    Tudo

---
+++ app/src/main/java/br/edu/unidavi/alfonso/android2todolistsqlite/MainActivity.java
@@ -0,0 +1,50 @@
+package br.edu.unidavi.alfonso.android2todolistsqlite;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
+
+import br.edu.unidavi.alfonso.android2todolistsqlite.data.DatabaseHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView taskList = null;
+    private FloatingActionButton buttonCreate = null;
+    private TasksAdapter adapter = new TasksAdapter();
+    private DatabaseHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helper = new DatabaseHelper(this);
+
+        taskList = findViewById(R.id.task_list);
+        taskList.setLayoutManager(new LinearLayoutManager(this));
+        taskList.setAdapter(adapter);
+
+        buttonCreate = findViewById(R.id.button_create);
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(), "Click", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), NewTaskActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        List<Task> tasks = helper.fetchTasks();
+        adapter.setup(tasks);
+    }
+}
---
+++ app/src/main/java/br/edu/unidavi/alfonso/android2todolistsqlite/NewTaskActivity.java
@@ -0,0 +1,38 @@
+package br.edu.unidavi.alfonso.android2todolistsqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import br.edu.unidavi.alfonso.android2todolistsqlite.data.DatabaseHelper;
+
+public class NewTaskActivity extends AppCompatActivity {
+
+    private Button buttonSave = null;
+    private EditText inputTask = null;
+    private DatabaseHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task);
+
+        helper = new DatabaseHelper(this);
+
+        buttonSave = findViewById(R.id.button_save);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                inputTask = findViewById(R.id.input_new_task);
+                String value = inputTask.getText().toString();
+                if (!value.isEmpty()) {
+                    helper.createTask(value);
+                    finish();
+                }
+            }
+        });
+    }
+}
